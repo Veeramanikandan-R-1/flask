7188cd08ab0ccdf778d842278fcdd1a19031bf85
Veeramanikandan-R-1 <r.veeramanikandany216@gmail.com>
2022-02-05 23:58:28 +0530

    resend otp functionality added

--- javatpoint/flask_mail_module/sending_mail.py
+++ javatpoint/flask_mail_module/sending_mail.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 # flask mail configuration
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT']=465
-app.config['MAIL_USERNAME']='manirshv98@gmail.com'
+app.config['MAIL_USERNAME']='rsvhmani98@gmail.com'
 app.config['MAIL_PASSWORD']='Mani123#'
 app.config['MAIL_USE_TLS']=False
 app.config['MAIL_USE_SSL']=True
